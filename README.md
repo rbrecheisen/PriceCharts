addd59eabe802b386ddfe6d280f6e4729dc77ccf
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-12-13 16:40:16 +0100

    Started experiment with OANDA REST API

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PriceCharts
-Web application that calculates and tracks price chart indicators with notifications about interesting combinations
+Web application that calculates and tracks price chart indicators with notifications about interesting combinations.
+
+Idea is to make an HTML page where you can retrieve candle stick information and provide parameters like:
+- CandleStickGranularity: e.g., S15, M15, H1, D, W, M
+- PricingComponent: e.g., M (midpoint), B (bid) and A (ask) or a combination of these (e.g., BM)
+- InstrumentName: EUR_USD
+
+When the candle stick information has been retrieved, the price chart is displayed.
--- experiments/oanda/main.py
+++ experiments/oanda/main.py
@@ -1,18 +1,46 @@
+import json
+import requests
+
 import oandapyV20
 import oandapyV20.endpoints.pricing as pricing
 
-API_KEY = '7825bb8dbc5a98a48c7dc393ec236b96-fe2ba1edb705afca6030491cfeb05c77'
-ACCOUNT_ID = '101-004-30581840-001'
-OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
 INSTRUMENT = 'EUR_USD'
+
+OANDA_APIKEY = '7825bb8dbc5a98a48c7dc393ec236b96-fe2ba1edb705afca6030491cfeb05c77'
+OANDA_ACCOUNTID = '101-004-30581840-001'
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+# OANDA_URL_CANDLES = f'{OANDA_URL}/accounts/{OANDA_ACCOUNTID}/candles/latest'
+OANDA_URL_CANDLES = f'{OANDA_URL}/accounts/{OANDA_ACCOUNTID}/instruments/{INSTRUMENT}/candles'
+OANDA_URL_PRICING = f'{OANDA_URL}/accounts/{OANDA_ACCOUNTID}/pricing'
+HEADERS = {'Authorization': f'Bearer {OANDA_APIKEY}', 'Content-Type': 'application/json'}
+
+
+def get_pricing(url, instrument):
+    response = requests.get(url, headers=HEADERS, params={'instruments': instrument})
+    if response.status_code == 200:
+        prices = response.json().get('prices', [])
+        for price in prices:
+            print(price)
+
+
+def get_candlesticks(url, instrument, granularity, start, stop):
+    response = requests.get(url, headers=HEADERS, params={
+        'instrumentName': instrument,
+        'pricingComponent': 'M',
+        'granularity': granularity,
+        'from': start,
+        'to': stop,
+    })
+    if response.status_code == 200:
+        info = response.json()
+        print(json.dumps(info, indent=4))
+    else:
+        print(response.status_code)
 
 
 def main():
-    client = oandapyV20.API(access_token=API_KEY)
-    pricing_info = pricing.PricingInfo(accountID=ACCOUNT_ID, params={'instruments': INSTRUMENT})
-    client.request(pricing_info)
-    prices = pricing_info.response['prices']
-    print(prices)
+    # A candlestick starts at 22:00 local time and ends at 21:59 local time.
+    get_candlesticks(OANDA_URL_CANDLES, 'EUR_USD', 'D', '2024-12-11T22:00Z', '2024-12-12T21:59Z')
 
 
 if __name__ == '__main__':
